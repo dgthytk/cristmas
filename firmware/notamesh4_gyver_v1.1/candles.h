ea664583678670e7526e19b04ea95578ad6adafc
dgthytk <154918393+dgthytk@users.noreply.github.com>
2023-12-26 21:37:22 +0300

    Update candles.h

--- firmware/notamesh4_gyver_v1.1/candles.h
+++ firmware/notamesh4_gyver_v1.1/candles.h
@@ -1,27 +1,24 @@
-#ifndef CANDLES_H
-#define CANDLES_H
+@@ -1,4 +1,4 @@
+﻿#ifndef  CANDLES_H
+#ifndef  CANDLES_H
+#define  CANDLES_H
 
-void candles() {
-  if (KolLed >= 10) {
-
-#if MAX_LEDS < 255
-    uint8_t x;
-#else
-    uint16_t x;
+пустые  свечи () {
+  если ( KolLed >= 10 ) {
+#if  MAX_LEDS  <  255
+    uint8_t  х ;
+#еще
+    uint16_t  х ;
 #endif
-
-    for (x = 0; x < KolLed; x++)
+    для ( x  =  0 ; x  <  KolLed ; x ++ )
     {
-#if CANDLE_40 == 1
-      //Вариант  efimova1969
-      uint8_t flicker = random8(1, 80);
-      leds[x] = CRGB(255 - flicker * 2, 150 - flicker, flicker / 2);
-#else
-      uint8_t flicker = random8(1, 15);
-      leds[x] = CRGB(255 - flicker * 2, 63 - flicker, flicker / 2);
+#if  CANDLE_40  ==  1
+      //Вариант ефимова1969
+      uint8_t  мерцание  =  случайный8 ( 1 , 80 );
+      светодиоды [ x ] =  CRGB ( 255  —  мерцание  *  2 , 150  —  мерцание , мерцание / 2 );
+#еще
+      uint8_t  мерцание  =  случайный8 ( 1 , 15 );
+      светодиоды [ x ] =  CRGB ( 255  —  мерцание  *  2 , 63  —  мерцание , мерцание / 2 );
 #endif
     }
   }
-} // candles()
-
-#endif
