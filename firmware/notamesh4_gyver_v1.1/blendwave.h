3c890578ba7dfbe2bf4c5d23cb2dfd9a778cd18f
dgthytk <154918393+dgthytk@users.noreply.github.com>
2023-12-26 21:34:00 +0300

    Update blendwave.h

--- firmware/notamesh4_gyver_v1.1/blendwave.h
+++ firmware/notamesh4_gyver_v1.1/blendwave.h
@@ -1,29 +1,26 @@
-#ifndef BLENDWARE_H
-#define BLENDWARE_H
+@@ -1,4 +1,4 @@
+﻿#ifndef  BLENDWARE_H
+#ifndef  BLENDWARE_H
+#define  BLENDWARE_H
 
-void blendwave() {
-  CRGB clr1;
-  CRGB clr2;
-  uint8_t speed;
-  uint8_t loc1;
-
-  if (KolLed >= 10) {
-
-    speed = beatsin8(6, 0, 255);
-
-    clr1 = blend(CHSV(beatsin8(3, 0, 255), 255, 255), CHSV(beatsin8(4, 0, 255), 255, 255), speed);
-    clr2 = blend(CHSV(beatsin8(4, 0, 255), 255, 255), CHSV(beatsin8(3, 0, 255), 255, 255), speed);
-
-#if MAX_LEDS < 255
-    loc1 = beatsin8(10, 0, KolLed - 1);
-#else
-    loc1 = beatsin16(10, 0, KolLed - 1);
+пустота  blendwave () {
+  CRGB  clr1 ;
+  CRGB  clr2 ;
+  uint8_t  скорость ;
+  uint8_t  loc1 ;
+  если ( KolLed >= 10 ) {
+    скорость  =  удары8 ( 6 , 0 , 255 );
+    clr1  =  blend ( CHSV ( beatsin8 ( 3 , 0 , 255 ), 255 , 255 ), CHSV ( beatsin8 ( 4 , 0 , 255 ), 255 , 255 ), скорость );
+    clr2  =  blend ( CHSV ( beatsin8 ( 4 , 0 , 255 ), 255 , 255 ), CHSV ( beatsin8 ( 3 , 0 , 255 ), 255 , 255 ), скорость );
+#if  MAX_LEDS  <  255
+    loc1  =  Beatsin8 ( 10 , 0 , KolLed  -  1 );
+#еще
+    loc1  =  Beatsin16 ( 10 , 0 , KolLed  -  1 );
 #endif
-
-    fill_gradient_RGB(leds, 0, clr2, loc1, clr1);
-    fill_gradient_RGB(leds, loc1, clr2, KolLed - 1, clr1);
+    fill_gradient_RGB ( светодиоды , 0 , clr2 , loc1 , clr1 );
+    fill_gradient_RGB ( leds , loc1 , clr2 , KolLed  -  1 , clr1 );
   }
-} // blendwave()
 
 
-#endif
+
+
