f44fa8f8768e2eaf25da306f1d66166828fd92a5
dgthytk <154918393+dgthytk@users.noreply.github.com>
2023-12-26 21:40:27 +0300

    Update rainbow_beat.h

--- firmware/notamesh4_gyver_v1.1/rainbow_beat.h
+++ firmware/notamesh4_gyver_v1.1/rainbow_beat.h
@@ -1,14 +1,14 @@
-#ifndef RAINBOW_BEAT_H
-#define RAINBOW_BEAT_H
+@@ -1,4 +1,4 @@
+﻿#ifndef  RAINBOW_BEAT_H
+#ifndef  RAINBOW_BEAT_H
+#define  RAINBOW_BEAT_H
 
-// Usage - rainbow_beat();
-
-void rainbow_beat() {                                         // The fill_rainbow call doesn't support brightness levels
-  if (KolLed >= 10) {
-    uint8_t beatA = beatsin8(17, 0, 255);                        // Starting hue
-    uint8_t beatB = beatsin8(13, 0, 255);
-    fill_rainbow(leds, KolLed, (beatA + beatB) / 2, 8);        // Use FastLED's fill_rainbow routine.
+// Использование - Rainbow_beat();
+void  Rainbow_beat () {                                          // Вызов fill_rainbow не поддерживает уровни яркости
+  если ( KolLed >= 10 ) {
+    uint8_t  BeatA  =  Beatsin8 ( 17 , 0 , 255 );                        // Начальный оттенок
+    uint8_t  BeatB  =  Beatsin8 ( 13 , 0 , 255 );
+    fill_rainbow ( leds , KolLed , ( beatA  +  BeatB )/ 2 , 8 );        // Используйте процедуру fill_rainbow FastLED.
   }
-} // rainbow_beat()
-
+} // Rainbow_beat()
 #endif
